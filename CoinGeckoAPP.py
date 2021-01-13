ac4c7c5f6feb99a6145019d22a4fae196994d45e
CollinNatterstad <collin.ragnarokarchery@gmail.com>
2021-01-12 18:05:56 -0800

    Update

--- CoinGeckoAPP.py
+++ CoinGeckoAPP.py
@@ -6,11 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_USER_PASS = os.getenv("DB_USER_PASS")
 DB_PORT = os.getenv ("DB_PORT")
 
 cg = CoinGeckoAPI()
+ids = ["Bitcoin","Polkadot","Ethereum"]
 
-CryptoPrice = cg.get_price(ids=['bitcoin, polkadot, ethereum'], vs_currencies= "usd", )
+CryptoPrice = cg.get_price(ids= ids, vs_currencies= "usd", )
+print(CryptoPrice)
 
+class databaseconnection:
+    try: 
+        conn = pg.connect(database = DB_NAME, user = DB_USER, password = DB_USER_PASS, host = DB_HOST, port = DB_PORT )
+        print ("Database Connection Successful")
+    
+    except:
+        print ("Database Connection Unsuccessful")
