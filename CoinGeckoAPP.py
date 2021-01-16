6830af35b3b6db6d927c8caea981df5d55675eb3
CollinNatterstad <collin.ragnarokarchery@gmail.com>
2021-01-16 15:16:27 -0800

    update

--- CoinGeckoAPP.py
+++ CoinGeckoAPP.py
@@ -1,45 +1,67 @@
-#importing libraries
-import os
-import json
-import psycopg2 as pg
-from pycoingecko import CoinGeckoAPI
 
-#importing dotenv for access to .env file
-from dotenv import load_dotenv
-load_dotenv()
+def main():
+    #importing libraries
+    import os
+    import json
+    import psycopg2 as pg
+    from pycoingecko import CoinGeckoAPI
 
-#connecting .env contents to a program variable
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_USER_PASS = os.getenv("DB_USER_PASS")
-DB_PORT = os.getenv ("DB_PORT")
+    #importing dotenv for access to .env file
+    from dotenv import load_dotenv
+    load_dotenv()
 
-#function containing api call. 
-def connect_API():
-    #shorting CoinGeckoAPI for easy access
-    cg = CoinGeckoAPI()
-    #list of interested coins. 
-    cryptolist = ["Bitcoin","Polkadot","Ethereum"]
+    #connecting .env contents to a program variable
+    DB_HOST = os.getenv("DB_HOST")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_USER_PASS = os.getenv("DB_USER_PASS")
+    DB_PORT = os.getenv ("DB_PORT")
+    
+    #function containing api call. 
+    def connect_bitcoin():
+        #shorting CoinGeckoAPI for easy access
+        cg = CoinGeckoAPI()
 
-    #for loop iterates over each coin and brings back pricing data. this data will then be manipulated and pushed to databases.     
-    for coin in cryptolist:
         #connects to coingecko through api, returns price for each coin.
-        CryptoPrice = cg.get_price(ids= coin, vs_currencies= "usd")
-        print(CryptoPrice)        
+        CryptoPrice = cg.get_price(ids= 'bitcoin', vs_currencies= "usd")
+        print(CryptoPrice)
+        #opens connection to postgres database through psycopg2 
+        conn = pg.connect(database = DB_NAME, user= DB_USER, password= DB_USER_PASS, host= DB_HOST, port= DB_PORT)
 
 
-def update_tables():
-    conn = pg.connect(database = DB_NAME, user= DB_USER, password= DB_USER_PASS, host= DB_HOST, port= DB_PORT)
+        #closes connection to postgres database
+        conn.close() 
+            
+    def connect_ethereum():
 
-    conn.close()
+        cg = CoinGeckoAPI()
+        CryptoPrice = cg.get_price(ids= 'ethereum', vs_currencies= "usd")
+        print(CryptoPrice)
+        conn = pg.connect(database = DB_NAME, user= DB_USER, password= DB_USER_PASS, host= DB_HOST, port= DB_PORT)
 
-#Operations involving local database. 
-connect_API()
-update_tables()
+        conn.close() 
+    def connect_polkadot():
+
+        cg = CoinGeckoAPI()
+        CryptoPrice = cg.get_price(ids= "polkadot", vs_currencies= "usd")
+        print(CryptoPrice)
+        conn = pg.connect(database = DB_NAME, user= DB_USER, password= DB_USER_PASS, host= DB_HOST, port= DB_PORT)
+
+        conn.close() 
+
+
+  
+
+    #function calls 
+    connect_bitcoin()
+    connect_ethereum()
+    connect_polkadot()
+    
 
 
 
+if __name__ == "__main__":
+    main()
 
 
 
