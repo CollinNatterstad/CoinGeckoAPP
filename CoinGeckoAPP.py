0a5dd215b4518fd935084ad7a85d046788594256
CollinNatterstad <collin.ragnarokarchery@gmail.com>
2021-01-22 00:11:42 -0800

    update

--- CoinGeckoAPP.py
+++ CoinGeckoAPP.py
@@ -1,9 +1,9 @@
 
 def main():
     #importing libraries
-    import os, json
+    import os
     import psycopg2 as pg
-    from psycopg2.extras import json
+    from psycopg2 import sql
     from pycoingecko import CoinGeckoAPI
     from datetime import date
 
@@ -18,41 +18,59 @@ def main():
     DB_USER_PASS = os.getenv("DB_USER_PASS")
     DB_PORT = os.getenv ("DB_PORT")
     
-    #function containing api call. 
-    def connect_API():
+  
+    def connect_and_store():
+
         #shorting CoinGeckoAPI for easy access
         cg = CoinGeckoAPI()
-        coins = ['Bitcoin', 'Ethereum','Polkadot']
+        coins = ['bitcoin', 'ethereum','polkadot']
+
         #connects to coingecko through api, returns price for each coin.
-        CryptoPrice = cg.get_price(ids= coins, vs_currencies= "usd")
+        CryptoPrice = cg.get_price(ids= coins, vs_currencies= "usd")        
 
-        now = date.today()
+        #For loop that seperates data from json format and stored into variable. key,value allows us to seperate the pair and pass key directly into the asset variable.
+        for key, value in CryptoPrice.items():
+ 
+            #The core data we are collecting.
+            #dictates which table data is directed towards.
+            asset = str(key)
+            call_value = value
 
-        for key,value in CryptoPrice.items():
+            print(asset)
+           
+                        
+            #price of asset takes both keys in cryptoprice json data and un-nests the price value. constructed as {keyone(asset):{keytwo(usd): pricevalue}}
+            price = CryptoPrice[f'{key}']['usd']
+            print(price)
+
+            #posted date of time. 
+            now = date.today()
+            print(now)
+
+            #opens connection to postgres database through psycopg2 
+            conn = pg.connect(database = DB_NAME, user= DB_USER, password= DB_USER_PASS, host= DB_HOST, port= DB_PORT)
             
+            #creating cursor to interact with database. 
+            cur = conn.cursor()
+            #confirming creation in terminal
+            print("\ncreated cursor object:", cur)
+            
+            cur.execute(
+                sql.SQL("insert into public.{} (date,price) values (%s, %s)")
+                    .format(sql.Identifier(f'{asset}')),[now,price]) 
+            print("Table Updated")
 
+            #commits changes to database  
+            conn.commit()
 
-        #opens connection to postgres database through psycopg2 
-        conn = pg.connect(database = DB_NAME, user= DB_USER, password= DB_USER_PASS, host= DB_HOST, port= DB_PORT)
-        #creating cursor to interact with database. 
-        curr = conn.cursor()
-        print("\ncreated cursor object:", curr)
+            #closes cursor
+            cur.close()
+            print("\nclosed cursor object:",cur)
 
-        #curr.execute("Insert INTO Bitcoin")
-
-
-
-        #commits changes to database
-        conn.commit()
-        #closes cursor
-        curr.close()
-        #closes connection to postgres database
-        conn.close()         
-
+            #closes connection to postgres database
+            conn.close()        
     #function calls 
-    connect_API()  
-
-
+    connect_and_store()
 
 if __name__ == "__main__":
     main()
